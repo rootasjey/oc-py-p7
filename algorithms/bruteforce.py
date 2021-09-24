a2533ebe508ef5ca556c9ff1b2e0b9dd1cf9c0fb
Jeremie Corpinot <rootasjey@users.noreply.github.com>
2021-09-24 10:12:03 +0000

    feat: ✨ handle float values

--- algorithms/bruteforce.py
+++ algorithms/bruteforce.py
@@ -10,11 +10,11 @@ def brute_force():
     print(f"CURRENT_MONEY = {CURRENT_MONEY}")
     print("-------")
 
-    # profit = 0
+    total_profit = 0
     all_actions = []
     chosen_actions = []
 
-    with open("data/dataset0.csv", newline="") as csv_file:
+    with open("data/dataset1.csv", newline="") as csv_file:
         spamreader = csv.reader(csv_file, delimiter=',', quotechar='|')
         for row in spamreader:
 
@@ -26,9 +26,12 @@ def brute_force():
 
         print("-------")
         chosen_actions = choose_actions(all_actions, CURRENT_MONEY, MAX_MONEY)
-        print(chosen_actions)
-
+        
         print(f"Picked {len(chosen_actions)} actions")
+        print("-------")
+        print(chosen_actions)
+        print("-------")
+
 
         total_cost = 0
 
@@ -36,8 +39,22 @@ def brute_force():
             cost = chosen_action[1]
             total_cost += cost
 
-        print(total_cost)
+        print(f"TOTAL COST: {total_cost}")
+        
+        total_profit = make_profit(chosen_actions)
+        print(f"TOTAL PROFIT: {total_profit}")
 
+
+
+def make_profit(actions):
+  """Calculate profit"""
+  total_profit = 0
+
+  for action in actions:
+    action_profit = action[2]
+    total_profit += action_profit
+
+  return total_profit
 
 def choose_actions(actions, money, max_money):
     """Pick actions"""
@@ -48,10 +65,16 @@ def choose_actions(actions, money, max_money):
 
     while money > 0 and cursor < (actions_len - 1):
         curr_action = actions[cursor]
-        chosen_actions.append(curr_action)
 
+        # Check if enough money to buy this action
         action_cost = curr_action[1]
-        money -= action_cost
+
+        future_money = money - action_cost
+
+        if (future_money >= 0):
+          chosen_actions.append(curr_action)
+          money -= action_cost
+
         cursor += 1
 
     return chosen_actions
@@ -60,18 +83,25 @@ def choose_actions(actions, money, max_money):
 def is_action_row(data):
     """Check if the row has int values."""
     has_int_values = False
+    has_float_values = False
 
     for value in data:
         try:
+          if '.' in value:
+            maybe_float = float(value)
+          
+            if isinstance(maybe_float, float):
+                has_float_values = True
+
+          else: 
             maybe_int = int(value)
 
             if isinstance(maybe_int, int):
                 has_int_values = True
         except:
             "This value is not an int."
-            #print(f"{value} is not an int.")
 
-    return has_int_values
+    return has_int_values or has_float_values
 
 
 def format_action_row(data):
@@ -79,8 +109,18 @@ def format_action_row(data):
     formatted_data = []
     formatted_data.append(data[0])
 
-    cost = int(data[1])
-    benefice_percent = int(data[2])
+    cost = 0
+    benefice_percent = 0
+
+    if '.' in data[1]:
+      cost = float(data[1])
+    else:
+      cost = int(data[1])
+
+    if '.' in data[2]:
+      benefice_percent = float(data[2])
+    else:
+      benefice_percent = int(data[2])
 
     formatted_data.append(cost)
     formatted_data.append(benefice_percent)
